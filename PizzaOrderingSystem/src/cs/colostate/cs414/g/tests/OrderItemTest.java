5a90444779787cdcad9e8b3a6dcab7fe7efe85ba
varsha0905 <varshaarun@Varshas-MacBook-Pro.local>
2014-10-24 14:02:34 -0600

    Added OrderItemTest

---
+++ PizzaOrderingSystem/src/cs/colostate/cs414/g/tests/OrderItemTest.java
@@ -0,0 +1,51 @@
+package cs.colostate.cs414.g.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import cs.colostate.cs414.g.domain.*;
+
+public class OrderItemTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testOrderItem() {
+		MenuItem menuItem = new MenuItem("Test", 4.99);
+		OrderItem item = new OrderItem(menuItem);
+		assertTrue(item.getItem() == menuItem);
+		assertTrue(item.getQuantity() == 1);
+	}
+
+	@Test
+	public void testIncrementItemQuantity() {
+		MenuItem menuItem = new MenuItem("Test", 4.99);
+		OrderItem item = new OrderItem(menuItem);
+		item.incrementItemQuantity();
+		assertTrue(item.getQuantity() == 2);
+	}
+
+	@Test
+	public void testEqualsMenuItem() {
+		MenuItem menuItem = new MenuItem("Test", 4.99);
+		OrderItem item = new OrderItem(menuItem);
+		assertTrue(item.equals(menuItem) == true);
+	}
+
+	@Test
+	public void testGetItem() {
+		MenuItem menuItem = new MenuItem("Test", 4.99);
+		OrderItem item = new OrderItem(menuItem);
+		assertTrue(item.getItem() == menuItem);
+	}
+
+}
