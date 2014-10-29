f8934285cb2407ba3afedc4e86cd79d577f7ba40
Harshil Shah <harshil.shah@colostate.edu>
2014-10-29 17:48:33 -0600

    whole Integration is done

--- IntegratedProject/src/cs/colostate/cs414/g/test/TestAll.java
+++ IntegratedProject/src/cs/colostate/cs414/g/test/TestAll.java
@@ -4,9 +4,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import cs.colostate.cs414.g.domain.Payment;
+
 
 @RunWith(Suite.class)
-@SuiteClasses({ OrderTest.class })
+@SuiteClasses({ OrderTest.class, MenuItemModificationTest.class, MenuItemTest.class, MenuTest.class, PaymentByCardTest.class, PaymentByCashTest.class, PaymentTest.class })
 public class TestAll {
 
 }
