848da24e19da418fc869d2a3bfb8666664207e1e
varsha0905 <varshaarun@Varshas-MacBook-Pro.local>
2014-11-11 12:11:26 -0700

    Created an empty Server class
    Please check the package naming

---
+++ PizzaOrderingSystemServer/gen/cs/cs414/a5/g/pizzaorderingsystemserver/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cs.cs414.a5.g.pizzaorderingsystemserver;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
