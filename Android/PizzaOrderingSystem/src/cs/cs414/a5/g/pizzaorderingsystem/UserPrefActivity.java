91785abb422af1c10782cfca0fe9912489c289d9
varsha0905 <varshaarun@Varshas-MacBook-Pro.local>
2014-11-12 18:27:16 -0700

    Created a basic android application
    Nothing fancy. I tried to understand how it works.

---
+++ Android/PizzaOrderingSystem/src/cs/cs414/a5/g/pizzaorderingsystem/UserPrefActivity.java
@@ -0,0 +1,15 @@
+package cs.cs414.a5.g.pizzaorderingsystem;
+
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class UserPrefActivity extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.user_pref_fragment);
+	}
+	
+}
--- PizzaOrderingSystemServer/gen/cs/cs414/a5/g/pizzaorderingsystemserver/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package cs.cs414.a5.g.pizzaorderingsystemserver;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
