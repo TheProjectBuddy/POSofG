ad43c71fb4979c73053aabd77e1ffb626f7fcf14
Sandeep Kasavaraju <sandeep.kasavaraju@gmail.com>
2014-11-14 13:08:47 -0700

    Uploaded supporting controllers examples

---
+++ Pizza(Sandeep)/SupportingClasses/Server.java
@@ -0,0 +1,34 @@
+package cs.cs414.a5.g.pizzaorderingsystemserver;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpServer;
+
+public class Server {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		
+		HttpServer server = HttpServer.create(new InetSocketAddress(8000), 8000);
+
+		MenuController menu = new MenuController();
+		
+		OrderController order= new OrderController();
+		
+		SignupController signup= new SignupController();
+		
+		SigninController signin=new SigninController();
+		
+		server.createContext("/order",order);
+		server.createContext("/menu",menu);
+		server.createContext("/signup",signup);
+		server.createContext("/signin",signin);
+		
+		server.start();
+	}
+
+}
