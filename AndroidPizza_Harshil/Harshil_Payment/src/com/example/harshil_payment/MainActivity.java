ac1ca017d18b7b7a08d323cf7444b5f79f0976c0
Harshil Shah <harshil.shah@colostate.edu>
2014-11-14 12:17:40 -0700

    Addition of few files on payment

--- AndroidPizza_Harshil/Harshil_Payment/src/com/example/harshil_payment/MainActivity.java
+++ AndroidPizza_Harshil/Harshil_Payment/src/com/example/harshil_payment/MainActivity.java
@@ -1,19 +1,36 @@
 package com.example.harshil_payment;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
+	
+	
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MainActivity.this, ThankYou.class);
+				startActivity(intent);
+			}
+		});
     }
-
+    
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,4 +50,7 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+    
+   
+   
 }
