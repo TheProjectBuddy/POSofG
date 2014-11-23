f74556f80d4dbbd907305498f8a5a7dab60b1a4b
varsha0905 <varshaarun@Varshas-MacBook-Pro.local>
2014-11-23 13:00:29 -0700

    PaymentFirst is changed

--- Android/PizzaOrderingSystemClient/src/cs/colostate/cs414/a5/g/pizzaorderingsystemclient/PaymentFirst.java
+++ Android/PizzaOrderingSystemClient/src/cs/colostate/cs414/a5/g/pizzaorderingsystemclient/PaymentFirst.java
@@ -1,7 +1,21 @@
 package cs.colostate.cs414.a5.g.pizzaorderingsystemclient;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 
 public class PaymentFirst extends Activity {
-
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Intent intent = getIntent();
+		double total = Double.parseDouble(intent.getStringExtra("TotalPrice"));
+		/**
+		 * To be removed later
+		 */
+		String temp = ""+total;
+		Log.w("Total in PaymentFirst", temp);
+		
+	}
 }
